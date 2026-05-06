b4eed35629d63a053b2ed303e16f273a70e5803a
unknown <you@example.com>
2026-05-06 17:05:56 +0500

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero';
 import InfiniteScroll from './components/InfiniteScroll/InfiniteScroll';
-import ScrollToTop from './components/ScrollToTop/ScrollToTop';
 import Footer from "./Components/Footer/Footer.jsx";
 import Offer from './Components/Offer/Offer.jsx';
 import OfferSights from './Components/OfferSights/OfferSights.jsx';
@@ -20,7 +19,7 @@ function App() {
             <InfiniteScroll />
             <Map/>
             <Footer/>
-            {/* <ScrollToTop /> */}
+     
         </div>
     );
 }
